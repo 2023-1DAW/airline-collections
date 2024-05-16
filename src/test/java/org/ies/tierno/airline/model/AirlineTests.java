8e2e4454e240be7f7f28cd8e319b3131864e0722
Mikel <mikelsanvi@gmail.com>
2024-05-16 12:53:00 +0200

    ejemplo de airolinea hardcodeada

---
+++ src/test/java/org/ies/tierno/airline/model/AirlineTests.java
@@ -0,0 +1,70 @@
+package org.ies.tierno.airline.model;
+
+import com.sun.source.util.Trees;
+import org.ies.tierno.airline.model.comparators.ByNifPassengerComparator;
+
+import java.util.*;
+
+public class AirlineTests {
+
+    private Airline airline = createAirline();
+
+
+    private Airline createAirline() {
+        Map<Integer, Flight> flights = new HashMap<>();
+        var passengers1 = new TreeSet<Passenger>(new ByNifPassengerComparator());
+        passengers1.add(
+                new Passenger("001", 1, List
+                        .of(
+                                new Luggage(1, "Maleta roja")
+                        )
+                )
+        );
+        passengers1.add(
+                new Passenger("002", 2, List
+                        .of(
+                                new Luggage(2, "Maleta azul")
+                        )
+                )
+        );
+        flights.put(
+                1,
+                new Flight(
+                        1,
+                        "Madrid",
+                        "Barcelona",
+                        new Date(),
+                        passengers1
+                )
+        );
+
+        var passengers2 = new TreeSet<Passenger>(new ByNifPassengerComparator());
+        passengers2.add(
+                new Passenger("001", 10, List
+                        .of(
+                                new Luggage(3, "Maleta roja")
+                        )
+                )
+        );
+
+        flights.put(
+                2,
+                new Flight(
+                        2,
+                        "Barcelona",
+                        "Madrid",
+                        new Date(),
+                        passengers2
+                )
+        );
+
+        return new Airline(
+                "Aerolínea del IES",
+                flights,
+                List.of(
+                        new Customer("001", "Bob", "Esponja"),
+                        new Customer("002", "Peppa", "Pig")
+                )
+        );
+    }
+}
